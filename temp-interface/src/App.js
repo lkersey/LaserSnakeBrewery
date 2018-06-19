15a2fc6ba957b52c2b449b2d1fc558e9e7b9063d
loren <kerlo392@student.otago.ac.nz>
2018-06-20 10:01:50 +1200

    First functional component

--- temp-interface/src/App.js
+++ temp-interface/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+import HelloWorld from './HelloWorld'
+import './App.css'
 
 const App = () => {
-  return (<div className="App">Hello world!</div>);
-}
+  return (
+    <div className="App">
+    <HelloWorld name="Matt"/>
+    </div>
+  );
+};
 
 export default App;
---
+++ temp-interface/src/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HelloWorld.css'
+
+const HelloWorld = props => {
+  return (<div className="HelloWorld">Hello {props.name}!</div>);
+}
+
+export default HelloWorld;
